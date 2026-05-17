b01f706fee4cd3e8c737052af0f178fdfe7eeeac
cat <673176230@qq.com>
2026-05-17 17:58:51 +0800

    处理 事件

--- src/util/utils.go
+++ src/util/utils.go
@@ -2,6 +2,8 @@ package util
 
 import (
 	"context"
+	"fmt"
+	"strings"
 	"time"
 )
 
@@ -14,4 +16,30 @@ func CheckError(err error) {
 
 func NewContext(timeoutSeconds int) (context.Context, context.CancelFunc) {
 	return context.WithTimeout(context.Background(), time.Duration(timeoutSeconds)*time.Second)
+}
+
+// 日志工具。
+type LogMaker struct {
+	lines []string
+}
+
+func (tt *LogMaker) AddKV(key string, value any) {
+	valueStr := fmt.Sprint(value)
+	line := key + " = " + valueStr
+	tt.AddLine(line)
+}
+func (tt *LogMaker) AddLine(line string) {
+	tt.lines = append(tt.lines, line)
+}
+func (tt *LogMaker) LogString() string {
+	buf := strings.Builder{}
+	buf.Grow(len(tt.lines) * 100)
+
+	for _, line := range tt.lines {
+		buf.WriteString(line)
+		buf.WriteString("\n")
+	}
+	str := buf.String()
+	Logger.Info(str)
+	return str
 }
