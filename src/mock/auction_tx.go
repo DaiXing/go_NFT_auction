1bc030a8be71d9d1ab6e4c426363b51c090e6ade
cat <673176230@qq.com>
2026-05-24 11:41:06 +0800

    加mock

--- src/mock/auction_tx.go
+++ src/mock/auction_tx.go
@@ -2,7 +2,9 @@ package mock
 
 import (
 	"math/big"
+	"strconv"
 
+	"github.com/ethereum/go-ethereum/common"
 	"my.nft.auction/src/bean"
 	"my.nft.auction/src/eth"
 	"my.nft.auction/src/util"
@@ -10,11 +12,35 @@ import (
 
 // 创建拍卖
 func TxCreateAuction(caller *eth.UserInfo, req *bean.CreateAuctionReq) {
+	logMaker := util.LogMaker{}
+	defer logMaker.LogString()
+	logMaker.AddLine(">> path 创建拍卖")
+
+	// 合约
+	nftAddr := common.HexToAddress(req.NftContract)
+
+	// 转数字。
+	tokenIdInt, err1 := strconv.ParseInt(req.TokenId, 10, 64)
+	util.CheckError(err1)
+	tokenIdInt2 := big.NewInt(tokenIdInt)
+
+	minPrice := big.NewInt(int64(req.MinPrice))
+	beginTime := big.NewInt(int64(req.BeginTime))
+	periodTime := big.NewInt(int64(req.PeriodTime))
+
+	logMaker.AddKV(" nftAddr ", nftAddr)
+	logMaker.AddKV(" tokenId ", tokenIdInt2)
+	logMaker.AddKV(" minPrice ", minPrice)
+	logMaker.AddKV(" beginTime ", beginTime)
+	logMaker.AddKV(" periodTime ", periodTime)
+
 	// 函数+入参
-	funcData, err := eth.AuctionABI.Pack("createAuction", req.NftContract, req.TokenId,
-		big.NewInt(int64(req.MinPrice)), big.NewInt(int64(req.BeginTime)),
-		big.NewInt(int64(req.PeriodTime)))
+	funcData, err := eth.AuctionABI.Pack("createAuction", nftAddr, tokenIdInt2,
+		minPrice, beginTime, periodTime)
+	logMaker.AddKV(" 创建 funcData error ", err)
 	util.CheckError(err)
 
+	// 交易。
 	eth.CallTx(eth.AuctionContractAddr, funcData, caller)
+	logMaker.AddLine(" 创建 交易 ")
 }
--- test/simple_test.go
+++ test/simple_test.go
@@ -3,13 +3,13 @@ package test
 import (
 	"testing"
 
+	"my.nft.auction/src/bean"
 	"my.nft.auction/src/util"
-	"my.nft.auction/src/web"
 )
 
 func TestHealth(tt *testing.T) {
 	tt.Log("测试：健康检测")
-	resp, err := util.HttpGetJson[web.BaseResp](URL_HEALTH)
+	resp, err := util.HttpGetJson[bean.BaseResp](URL_HEALTH)
 	if err != nil {
 		tt.Fatal("URL_HEALTH error", err)
 	}
@@ -19,13 +19,13 @@ func TestHealth(tt *testing.T) {
 
 func TestQueryToken(tt *testing.T) {
 	tt.Log("测试：查询token列表")
-	req := web.GetTokenListReq{
+	req := bean.GetTokenListReq{
 		Seller: "0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045",
-		BaseReq: web.BaseReq{
+		BaseReq: bean.BaseReq{
 			PageSize: 2,
 		},
 	}
-	resp, err := util.HttpPostJson[web.GetTokenListReq, web.GetTokenListResp](URL_GET_TOKEN_LIST, &req)
+	resp, err := util.HttpPostJson[bean.GetTokenListReq, bean.GetTokenListResp](URL_GET_TOKEN_LIST, &req)
 	if err != nil {
 		tt.Fatal("URL_GET_TOKEN_LIST error", err)
 	}
